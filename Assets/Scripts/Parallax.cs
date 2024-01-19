7bc2c9a16c21c730c18ac675b3f0ac5fcdef8759
IWishMilkCookies <jonas.bruylant@gmail.com>
2024-01-19 22:05:18 +0100

    Removed prior assets, added new tileset, added parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] Transform camTransform;
+    [SerializeField] float relativeMove = .3f;
+    [SerializeField] bool lockY = false;
+
+
+    void Update()
+    {
+        if (lockY)
+            transform.position = new Vector2(camTransform.position.x * relativeMove, camTransform.position.y * relativeMove);
+        else
+            transform.position = new Vector2(camTransform.position.x * relativeMove, camTransform.position.y);
+    }
+}
